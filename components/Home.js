d0f38ce04e8c1f28379829889fe37e77ffcf2f59
huzaifa003 <buntyhuzaifa3@gmail.com>
2023-09-19 01:03:32 +0500

    Fixed Bug

--- components/Home.js
+++ components/Home.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default Home = ()=>{
     return (
       <View className="flex-1 items-center justify-center bg-white">
-          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Open up App.js t working on your app!</Text>
           <StatusBar style="auto" />
         </View>
       );
