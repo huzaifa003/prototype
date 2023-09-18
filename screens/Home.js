c3cd62e91d881561ff149258e7bf56165a7cb8dc
arsalanbashir831 <arsalanbashir831@gmail.com>
2023-09-19 03:24:52 +0500

    Home Screen Done

--- screens/Home.js
+++ screens/Home.js
@@ -1,51 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, ImageBackground } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
 export default Home = () => {
   return (
     <>
-      <LinearGradient colors={['#ef32d9', '#89fffd']} style={{ height: '100%' }}>
-        <Text />
-        <Text />
-        <Text />
-
-        <View className="flex-1 items-center justify-top " style={{maxHeight: "30%"}}>
-          <Image source={require('../assets/skainetLogo-removebg-preview.png')} />
+      <ImageBackground
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100%",
+        }}
+        source={require("../assets/bgImage.png")}
+      >
+        <View
+          className="flex-1 items-center justify-top "
+          style={{ maxHeight: "30%" }}
+        >
+          <Image
+            source={require("../assets/skainetLogo-removebg-preview.png")}
+          />
 
           <StatusBar style="auto" />
         </View>
 
-        <View className="flex-1 items-center justify" style={{maxHeight: "20%"}}>
-          <Text style={{ paddingLeft: '10%', paddingRight: '10%' }} className="text-3xl antialiased"> Easily Connect with your family, friends and work through skainet </Text>
-          <Text></Text>
-        
-          
+        <View
+          className="flex-1 items-center justify"
+          style={{ maxHeight: "20%" }}
+        >
+          <Text
+            style={{ paddingLeft: "10%", paddingRight: "10%" }}
+            className="text-3xl antialiased text-white"
+          >
+            {" "}
+            Easily Connect with your family, friends and work through skainet{" "}
+          </Text>
+          <Text className=' w-[100px] h-1 mt-7 bg-[#1B97DD]'></Text>
         </View>
 
         <View
-            style={{
-              marginLeft: '30%',
-              alignItems : 'center',
-              justifyContent: 'center',
-              borderWidth: 0.9,
-              borderColor: 'blue',
-              margin: 0,
-              maxWidth: "40%"
-            }}
-          />
-
-
-
-      </LinearGradient>
+          style={{
+            marginLeft: "30%",
+            alignItems: "center",
+            justifyContent: "center",
+            borderWidth: 0.9,
+            borderColor: "blue",
+            margin: 0,
+            maxWidth: "40%",
+          }}
+        />
+      </ImageBackground>
     </>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
