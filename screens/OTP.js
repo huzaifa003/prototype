4b27121fcec84f75218f2f254449bd5450cee3cc
arsalanbashir831 <arsalanbashir831@gmail.com>
2023-09-19 20:15:31 +0500

    debugging opt

--- screens/OTP.js
+++ screens/OTP.js
@@ -1,133 +1,105 @@
-import { View, Text, ImageBackground, Image, TextInput, TouchableOpacity, Linking } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text, ImageBackground, Image, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { LinearGradient } from "expo-linear-gradient";
+import { LinearGradient } from 'expo-linear-gradient';
 
 const OTP = ({ navigation }) => {
   return (
-    <>
-      <ImageBackground
+    <ImageBackground
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        height:'auto'
+      }}
+      source={require('../assets/bgImage.png')}
+    >
+    
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
         style={{
           flex: 1,
-          alignItems: "center",
-          justifyContent: "space-around",
-          height: "100%",
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+          
         }}
-        source={require("../assets/bgImage.png")}
       >
-        <View style={{height: '100%', alignSelf: 'center', alignItems: 'center', justifyContent: 'center'}} className="w-[100%] h-[100%%] self-center"> 
-          <LinearGradient
-            className="w-[90%] h-[60%] place-content-center self-cente rounded-md shadow-sm  "
-            start={{ x: 0, y: 0 }}
-            end={{ x: 1, y: 1 }}
-            colors={["#171717CC", "#1D1E24"]}
+        <LinearGradient
+          style={{
+            width: '90%',
+            height: '60%',
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: 10,
+            shadowColor: '#000',
+            shadowOffset: {
+              width: 0,
+              height: 2,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 4,
+            elevation: 5,
+          }}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 1 }}
+          colors={['#171717CC', '#1D1E24']}
+        >
+          <View>
+            <Image
+              style={{ alignSelf: 'center' }}
+              source={require('../assets/sn_logo-removebg-preview.png')}
+            />
+             <Text
+            style={{ opacity: 0.6 }}
+            className="text-center text-white text-xl"
           >
-
-            <View className="rounded-md  ">
-              <Image
-                style={{ alignSelf: "center" }}
-                source={require("../assets/sn_logo-removebg-preview.png")}
-              />
-
-              <Text
-                style={{ opacity: 0.6 }}
-                className="text-center text-[#D7D7D7] mt-10 mb-10 text-lg"
-              >
-                Enter the 4 Digits pin sent to you
-              </Text>
-              <View style={{ flex: 1, flexDirection: 'row', minHeight: '15%', height: "20%", alignContent: 'center', alignSelf: 'center', flexBasis: 'auto', justifyContent: 'space-between' }}>
-                <TextInput
-                  style={{
-                    maxHeight: '50%',
-                    minHeight: '50%',
-                    maxWidth: "15%",
-                    borderWidth: 1,
-                    borderColor: "white",
-                    color: "white",
-
-                  }}
-                  className="bg-[#1D1D1F] text-white rounded-sm m-2 p-2 pl-5 "
-                ></TextInput>
-
-                <TextInput
-                  style={{
-                    maxHeight: '50%',
-                    minHeight: '50%',
-                    maxWidth: "15%",
-                    borderWidth: 1,
-                    borderColor: "white",
-                    color: "white",
-                  }}
-                  className="bg-[#1D1D1F] text-white rounded-sm m-2 p-2 pl-5 "
-                ></TextInput>
-
-                <TextInput
-                  style={{
-                    maxHeight: '50%',
-                    minHeight: '50%',
-                    maxWidth: "15%",
-                    borderWidth: 1,
-                    borderColor: "white",
-                    color: "white",
-                  }}
-                  className="bg-[#1D1D1F] text-white rounded-sm m-2 p-2 pl-5 "
-                ></TextInput>
-
-                <TextInput
-                  style={{
-                    maxHeight: '50%',
-                    minHeight: '50%',
-                    maxWidth: "15%",
-                    borderWidth: 1,
-                    borderColor: "white",
-                    color: "white",
-                  }}
-                  className="bg-[#1D1D1F] text-white rounded-sm m-2 p-2 pl-5 "
-                ></TextInput>
-
-
-
-              </View>
-
-              <TouchableOpacity onPress={() => { navigation.navigate('OTP') }} className=" rounded-md m-5  w-[50%] self-center ">
-                <LinearGradient
-                  start={{ x: 0, y: 0 }}
-                  end={{ x: 1, y: 1 }}
-                  colors={["#3f5efb", "#fc466b"]}
-                >
-                  <Text className="text-center text-white p-2 rounded-md">
-                    Confirm
-                  </Text>
-                </LinearGradient>
-              </TouchableOpacity>
-
-              <Text
-                style={{ opacity: 0.8, fontWeight :'300', color: 'white' }}
-                className="text-center  mb-10 text-md"
-              >
-
-                Resend a New Pin
-              </Text>
-            </View>
-
-
-          </LinearGradient>
-
-
-          <Text
-            style={{ color: 'white' }}
-            className = " mt-10"
-          >
-
-            Already Have an Account?  <Text style={{color : '#ADD8E6', backgroundColor : 'transparent', textDecorationLine: 'underline', textDecorationColor : '#ADD8E6'}}> Sign Up </Text>
+            Get Your Ideas High
           </Text>
-        </View>
-      </ImageBackground>
-    </>
-
-
+            <Text style={{ opacity: 0.6, textAlign: 'center', color: '#D7D7D7', fontSize: 16, marginTop: 20, marginBottom: 20 }}>
+              Enter the 4 Digits pin sent to you
+            </Text>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+              {[1, 2, 3, 4].map((index) => (
+                <TextInput
+                  key={index}
+                  style={{
+                    width: '15%',
+                    borderWidth: 1,
+                    borderColor: 'white',
+                    color: 'white',
+                    borderRadius: 5,
+                    textAlign: 'center',
+                    fontSize: 18,
+                  }}
+                  keyboardType="numeric"
+                />
+              ))}
+            </View>
+            <TouchableOpacity onPress={() => { navigation.navigate('ChatList') }} style={{ marginTop: 20 }}>
+              <LinearGradient
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 1 }}
+                colors={['#3f5efb', '#fc466b']}
+                style={{ borderRadius: 10 }}
+              >
+                <Text style={{ color: 'white', padding: 10, textAlign: 'center', fontSize: 18, fontWeight: 'bold' }}>
+                  Confirm
+                </Text>
+              </LinearGradient>
+            </TouchableOpacity>
+            <Text style={{ opacity: 0.8, fontWeight: '300', color: 'white', textAlign: 'center', padding:10, fontSize: 16 }}>
+              Resend a New Pin
+            </Text>
+          </View>
+        </LinearGradient>
+        <Text style={{ color: 'white', marginTop: 20 }}>
+          Already Have an Account?{' '}
+          <Text style={{ color: '#ADD8E6', textDecorationLine: 'underline' }}>Sign Up</Text>
+        </Text>
+      </KeyboardAvoidingView>
+    </ImageBackground>
   );
+};
 
-}
-
-export default OTP
+export default OTP;
