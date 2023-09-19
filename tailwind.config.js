a12bd6a432ce5e392a27cfc99950016dd75efe34
huzaifa003 <buntyhuzaifa3@gmail.com>
2023-09-19 15:23:27 +0500

    temp

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,10 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}", "/screens/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {}
+    extend: {},
   },
   plugins: [],
 }
+
+  
 
